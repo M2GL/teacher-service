430bf3ee94efa7ab58847c2174a56fbeaab9043b
AMIOUR Billel <b.amiour@gpcdz.com>
2021-03-27 14:35:34 +0100

    some changes

--- src/main/java/dz/m2gl/al/TeacherServiceApplication.java
+++ src/main/java/dz/m2gl/al/TeacherServiceApplication.java
@@ -2,8 +2,10 @@ package dz.m2gl.al;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class TeacherServiceApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/dz/m2gl/al/api/TeacherController.java
@@ -0,0 +1,24 @@
+package dz.m2gl.al.api;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class TeacherController {
+	  @GetMapping("/teachers")
+	  public ResponseEntity getAll() {
+	       
+
+	        if (true) {
+	          
+	            return ResponseEntity.ok("list of teachers");
+	        }
+	        return ResponseEntity.notFound().build();
+
+
+	    }
+
+}
